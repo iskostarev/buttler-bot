45880d95df58b9ce896ded4e4a02e600baa2104a
Ivan Kostarev <iskostarev@ya.ru>
2023-04-20 20:54:48 +0300

    update deps again

--- go.mod
+++ go.mod
@@ -5,15 +5,19 @@ go 1.19
 require (
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.9.0
-	maunium.net/go/mautrix v0.14.0
+	maunium.net/go/mautrix v0.15.1
 )
 
 require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/rs/zerolog v1.29.1 // indirect
 	github.com/tidwall/gjson v1.14.4 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
 	github.com/tidwall/sjson v1.2.5 // indirect
-	golang.org/x/crypto v0.6.0 // indirect
-	golang.org/x/net v0.7.0 // indirect
-	golang.org/x/sys v0.5.0 // indirect
+	golang.org/x/crypto v0.8.0 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
+	maunium.net/go/maulogger/v2 v2.4.1 // indirect
 )
--- vendor/maunium.net/go/mautrix/event/encryption.go
+++ vendor/maunium.net/go/mautrix/event/encryption.go
@@ -41,6 +41,7 @@ type EncryptedEventContent struct {
 	OlmCiphertext    OlmCiphertexts `json:"-"`
 
 	RelatesTo *RelatesTo `json:"m.relates_to,omitempty"`
+	Mentions  *Mentions  `json:"m.mentions,omitempty"`
 }
 
 type OlmCiphertexts map[id.Curve25519]struct {
@@ -92,6 +93,10 @@ type RoomKeyEventContent struct {
 	RoomID     id.RoomID    `json:"room_id"`
 	SessionID  id.SessionID `json:"session_id"`
 	SessionKey string       `json:"session_key"`
+
+	MaxAge      int64 `json:"com.beeper.max_age_ms"`
+	MaxMessages int   `json:"com.beeper.max_messages"`
+	IsScheduled bool  `json:"com.beeper.is_scheduled"`
 }
 
 // ForwardedRoomKeyEventContent represents the content of a m.forwarded_room_key to_device event.
@@ -101,6 +106,10 @@ type ForwardedRoomKeyEventContent struct {
 	SenderKey          id.SenderKey `json:"sender_key"`
 	SenderClaimedKey   id.Ed25519   `json:"sender_claimed_ed25519_key"`
 	ForwardingKeyChain []string     `json:"forwarding_curve25519_key_chain"`
+
+	MaxAge      int64 `json:"com.beeper.max_age_ms"`
+	MaxMessages int   `json:"com.beeper.max_messages"`
+	IsScheduled bool  `json:"com.beeper.is_scheduled"`
 }
 
 type KeyRequestAction string
@@ -134,6 +143,8 @@ const (
 	RoomKeyWithheldUnauthorized RoomKeyWithheldCode = "m.unauthorised"
 	RoomKeyWithheldUnavailable  RoomKeyWithheldCode = "m.unavailable"
 	RoomKeyWithheldNoOlmSession RoomKeyWithheldCode = "m.no_olm"
+
+	RoomKeyWithheldBeeperRedacted RoomKeyWithheldCode = "com.beeper.redacted"
 )
 
 type RoomKeyWithheldEventContent struct {
